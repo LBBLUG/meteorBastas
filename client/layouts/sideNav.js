d1ac119278de13fe9af268c50de28a166762a208
Brian <brian.mcgonagill@tylertech.com>
2016-06-24 07:14:56 -0500

    Updated menu, grids, and layout
    Fixed menu for now … though I don’t like it, and updated the way I’m
    using grids to help layout.

--- client/layouts/sideNav.js
+++ client/layouts/sideNav.js
@@ -9,6 +9,13 @@ Template.sideNav.events({
     'click  #sideMenu' (event) {
         event.preventDefault();
 
+        // TODO when a menu item is clicked, get the id of the 
+        // item and route to that id.
+        // i hate this right now, because the div id is
+        // picked up, but the click directly on the Icon
+        // doesn't give an id unless I set it the same.
+        // I've tried several things to get the parent node
+        // id with no luck so far.
         var clickedTarget = event.target.id;
         console.log("User clicked: " + clickedTarget);
         FlowRouter.go('/' + clickedTarget);
