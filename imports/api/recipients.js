6cc20c440f052695b08af7ae044f1c64d2a4accf
Brian <brian.mcgonagill@tylertech.com>
2016-06-27 07:20:52 -0500

    adding recipient methods

---
+++ imports/api/recipients.js
@@ -0,0 +1,31 @@
+miport { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+
+export const Recipients = new Mongo.Collection('recipients');
+
+Meteor.methods({
+    // insert a new Recipients
+    
+
+
+
+
+    // insert a gift for a recipient into a recipient document
+
+
+
+    // edit a recipient
+
+
+
+    // edit a gift
+
+
+
+    // insert a giver for gifts
+
+
+
+    // edit a giver for gifts
+});
