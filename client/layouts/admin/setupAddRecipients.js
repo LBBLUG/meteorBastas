1f60c9ff1f79ab089660960e6b4913c39d531e20
Brian McGonagill <brian.mcgonagill@tylertech.com>
2016-06-29 16:53:45 -0500

    no major change, just don't want library out of sync on other systems.

--- client/layouts/admin/setupAddRecipients.js
+++ client/layouts/admin/setupAddRecipients.js
@@ -1,6 +1,10 @@
 Template.setupAddRecipients.events({
     'click #submitAddRecipient' (event) {
         event.preventDefault();
+
+        var bastasId = $("#bastasId").val();
+        conssole.log("Bastas ID: " + bastasId);
+
         $('.ui.modal').modal('show');
     },
     'click #cancelAddRecipient' (event) {
