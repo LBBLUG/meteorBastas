e99ea96adb0c44673bf5ece1b4343b3cc19c82d6
bmcgonag <bmcgonag@gmail.com>
2017-06-25 08:27:56 -0500

    adding email section

---
+++ client/layouts/admin/sendReminder/sendReminder.js
@@ -0,0 +1,15 @@
+Template.sendReminder.onCreated(function() {
+
+});
+
+Template.sendReminder.onRendered(function() {
+
+});
+
+Template.sendReminder.helpers({
+
+});
+
+Template.sendReminder.events({
+
+});
