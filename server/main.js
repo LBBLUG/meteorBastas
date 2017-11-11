fa8241f5a69a0a70dd23cea18a952653bf9015af
bmcgonag <bmcgonag@gmail.com>
2017-11-11 13:38:14 -0600

    catch startup error

--- server/main.js
+++ server/main.js
@@ -25,24 +25,34 @@ import { MessagingSettings } from '../imports/api/messagingSettings.js';
 
 Meteor.startup(() => {
     // code to run on server at startup
-    let msgSettings = MessagingSettings.findOne({ active: true });
-    if (typeof msgSettings == 'undefined' || msgSettings == null || msgSettings == "") {
-      // msg settings not set, route user to setup for message settings.
-      console.log("Didn't find email settings.");
-    } else {
-        console.log("Found email settings");
-        let user = msgSettings.emailUser;
-        console.log("User = " + user);
-        if (msgSettings) {
-            smtp = {
-                username: msgSettings.emailUser,
-                password: msgSettings.emailPasswd,
-                server: msgSettings.smtpSrvUrl,
-                port: msgSettings.smtpPort
-            }
-
-            process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+    try {
+        let msgSettings = MessagingSettings.findOne({ active: true });
+        if (typeof msgSettings == 'undefined' || msgSettings == null || msgSettings == "") {
+          // msg settings not set, route user to setup for message settings.
+          // console.log("Didn't find email settings.");
+        } else {
+            // console.log("Found email settings");
+            let user = msgSettings.emailUser;
+            // console.log("User = " + user);
+            Meteor.call('setEmailFromServer', msgSettings);
         }
+    } catch (error) {
+        console.log("Error caught in server/main.js: " + error);
     }
 
+ });
+
+ Meteor.methods({
+     'setEmailFromServer' (msgSettings) {
+         if (msgSettings) {
+             smtp = {
+                 username: msgSettings.emailUser,
+                 password: msgSettings.emailPasswd,
+                 server: msgSettings.smtpSrvUrl,
+                 port: msgSettings.smtpPort
+             }
+
+             process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+         }
+     },
  });
