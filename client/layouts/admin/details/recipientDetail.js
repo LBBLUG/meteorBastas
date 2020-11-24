571d142f4eed0e7e62b20b5a783dc3aad6cbc544
Brian McGonagill <bmcgonag@gmail.com>
2020-11-23 19:08:45 -0600

    Added short setTimeout to allow page to load before select

--- client/layouts/admin/details/recipientDetail.js
+++ client/layouts/admin/details/recipientDetail.js
@@ -23,7 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import { Recipients } from '../../../../imports/api/recipients.js';
 
 Template.recipientDetailUpdate.onRendered(function() {
-    $('select').formSelect();
+    setTimeout(function() {
+        $('select').formSelect();
+    }, 250);
 });
 
 Template.recipientDetailUpdate.events({
